91a36f61a4dc7a1d17c8c2fad0825d7b95408d31
Victor amorim <victoramorimchagas@gmail.com>
2024-07-29 17:41:34 -0300

    Implement create posts feature for blog

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('blog')->group(function () {
+        Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+        Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    });
+});
+
+Route::prefix('blog')->group(function () {
+    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 });
 
 require __DIR__.'/auth.php';
---
+++ tests/Feature/PostControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_guest_cannot_access_the_create_post_page()
+    {
+        $response = $this->get(route('posts.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_access_the_create_post_page()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('posts.create'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('posts.create');
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_a_post()
+    {
+        $user = User::factory()->create();
+
+        $postData = [
+            'title' => 'Test Post',
+            'content' => 'This is a test post content.'
+        ];
+
+        $response = $this->actingAs($user)->post(route('posts.store'), $postData);
+
+        $response->assertRedirect(route('posts.index'));
+        $this->assertDatabaseHas('posts', $postData);
+    }
+
+    /** @test */
+    public function it_shows_all_posts_on_index_page()
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->get(route('posts.index'));
+
+        $response->assertStatus(200);
+        $response->assertSee($post->title);
+    }
+}
